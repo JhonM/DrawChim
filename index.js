37865d1f1e6206c2dd8c98ab4e2f69ccd72f4d9e
Jhon Majoor <jhon@blancframe.com>
2016-01-15 00:52:18 +0100

    WIP: setup prototype drawchim

--- index.js
+++ index.js
@@ -3,19 +3,30 @@
 
 var Multiply = require('./src/multiply');
 
-var jsBplate = function() {
-    if (!(this instanceof jsBplate)) {
-      return new jsBplate();
+var drawChim = function(elm) {
+    if (!(this instanceof drawChim)) {
+      return new drawChim();
     }
+
+    this.elm = elm;
+    this.elm.width = window.innerWidth;
+    this.elm.height = window.innerHeight;
+    
+    this.ctx = elm.getContext('2d');
+
+    this._init();
 };
 
-jsBplate.prototype.total = function(val) {
-    var amount = Multiply(val); 
-    console.log(amount);
-};
+drawChim.prototype._init = function() {
+    this.createCanvas();
+}
 
-jsBplate.prototype.subtract = function(val) {
-    return val - 5;
-};
+drawChim.prototype.createCanvas = function() {
+    this.ctx.fillStyle = '#000';
+    this.ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+    this.ctx.lineWidth = 6;
+    this.ctx.lineCap = 'round';
+    this.ctx.strokeStyle = 'rgba(0,0,255,0.5)';
+}
 
-module.exports = jsBplate;
+module.exports = drawChim;
